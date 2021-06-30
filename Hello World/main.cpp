f2a9cca1a421dc9de9d2799f6106c3dadda2b4e2
avindad2 <avindad2@illinois.edu>
2021-06-30 13:26:20 -0700

    Upload Files

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Avinda De Silva on 10/3/18.
+//  Copyright © 2018 Avinda De Silva. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "This is my first C++ project!\n";
+    return 0;
+}
